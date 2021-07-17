2ce1a85b69eea67097aaad9d39f5fefe85d253a8
valkiboy <val_kik@mail.ru>
2021-07-17 19:04:37 +0300

    Третья попытка

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,19 @@
 const hotelSearh = document.querySelector(".hotel-search");
 const selectingPopup = document.querySelector(".selecting");
+const selectingForm = document.querySelector(".selecting-form");
+const selectingFormcheckindate = document.querySelector(".selecting-form-check-in-date");
+const selectingFormdeparturedate = document.querySelector(".selecting-form-departure-date");
+const selectingFormadults = document.querySelector(".selecting-form-adults");
+const selectingFormchildren = document.querySelector(".selecting-form-children");
 
 hotelSearh.addEventListener("click", function () {
   selectingPopup.classList.toggle("modal-hide");
-})
+  selectingPopup.classList.remove("modal-error");
+});
+
+selectingForm.addEventListener("submit", function (evt) {
+  if (!selectingFormcheckindate.value || !selectingFormdeparturedate.value || !selectingFormadults.value || !selectingFormchildren.value) {
+    evt.preventDefault();
+    selectingPopup.classList.add("modal-error")
+  }
+});
